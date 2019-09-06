166cc6182dfa3a23eea36a7714ce8924a23ce76c
kavinanda <54997876+kavinanda@users.noreply.github.com>
2019-09-07 00:05:39 +0530

    Update Plot 1.R

--- Course Project 1 Solution/Plot 1.R
+++ Course Project 1 Solution/Plot 1.R
@@ -4,7 +4,7 @@ power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 #Naming the values
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
-#Subsetting power consumption
+#Subsetting the data
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
 #Plotting the histogram
